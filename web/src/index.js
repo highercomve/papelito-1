1c3f336a37dcf3f85903c2a1787b428480a893ce
Sergio Marin <higher.vnf@gmail.com>
2018-03-14 02:31:35 -0300

    Start hybrid for react native and web

--- web/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-import registerServiceWorker from "./registerServiceWorker";
-import "./css/style.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
