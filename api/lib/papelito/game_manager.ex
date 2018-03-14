dac141d5a2272c89e850968461dc76e2e0b79d00
Gustavo Gimenez <gimenezanderson@gmail.com>
2018-03-14 00:52:19 -0300

    require logger

--- api/lib/papelito/game_manager.ex
+++ api/lib/papelito/game_manager.ex
@@ -1,4 +1,7 @@
 defmodule Papelito.GameManager do
+  require Logger
+
+  alias Papelito.GamePlay
 
   def new_game(subject) do
     game_name = Haikunator.build
@@ -9,7 +12,15 @@ defmodule Papelito.GameManager do
   end
 
   def delete_game(game_name) do
-   Logger.info "Deleting game #{game_name}"
+    Logger.info "Deleting game #{game_name}"
     Papelito.Server.Game.terminate(game_name)
+  end
+
+  def save_game_log(game_name) do
+    Papelito.Server.Game.summary(game_name)
+    |> build_game_log()
+  end
+
+  def build_game_log(%GamePlay{} = game) do
   end
 end
